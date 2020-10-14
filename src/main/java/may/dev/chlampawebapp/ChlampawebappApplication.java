62f9cc71b4c9663456f53dee6ab9ab7fd632541f
root <root@may.may>
2020-10-14 20:58:37 +0100

    Starting the Spring Framework Guru Series

---
+++ src/main/java/may/dev/chlampawebapp/ChlampawebappApplication.java
@@ -0,0 +1,13 @@
+package may.dev.chlampawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChlampawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChlampawebappApplication.class, args);
+	}
+
+}
